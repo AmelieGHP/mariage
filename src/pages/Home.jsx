bc969b8847b87f9836f5b50cb0e68e2ece7738a1
Amélie Guilloux <109797891+AmelieGHP@users.noreply.github.com>
2023-05-10 20:39:02 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,6 @@ import weddingCar from "../assets/weddingCar.png";
 import { NavLink } from "react-router-dom";
 
 export default function Home() {
-//   console.log(
-//     "Coucou les Dave! Pour les motivés, Nathalie propose de vous accueillir pour un barbecue entre le cocktail et le bal, vu qu'elle habite à côté! Voir avec elle pour l'orga!"
-//   );
   return (
     <div className="homePage">
       <div className="pageContent">
@@ -35,60 +32,6 @@ export default function Home() {
               photos de l'évènement, dans la rubrique{" "}
               <NavLink to="/photos">Photos</NavLink>.
             </p>
-//             <p>
-//               {" "}
-//               Nous espérons que vous trouverez ici toutes les informations dont
-//               vous aurez besoin pour profiter au mieux de cet évènement à nos
-//               côtés.{" "}
-//             </p>
-//             <p>
-//               Vous pouvez nous confirmer votre présence, ou nous contacter pour
-//               toute question ou remarque, par mail:
-//             </p>
-//             <p className="accentText boldText">
-//               amelie-et-ferdinand@hotmail.com
-//             </p>
-            <p>Bonne navigation et à bientôt!</p>
-          </div>
-        </div>
-//         <div className="container programmeBox">
-//           <h3>Programme</h3>
-//           <div className="bubbles">
-//             <div className="programme">
-//               <img src={couple} alt="mairie" />
-//               <p>14h00</p>
-//               <p className="accentText">Mairie</p>
-//             </div>
-//             <div className="programme">
-//               <img src={church} alt="église" />
-//               <p>15h00</p>
-//               <p className="accentText">Église</p>
-//             </div>
-//             <div className="programme">
-//               <img src={weddingCar} alt="voiture" />
-//               <p>16h30</p>
-//               <p className="accentText">En route</p>
-//               <small>vers le domaine</small>
-//             </div>
-//             <div className="programme">
-//               <img src={wine} alt="cocktail" />
-//               <p>17h00</p>
-//               <p className="accentText">Cocktail</p>
-//             </div>
-//             <div className="programme">
-//               <img src={banquet} alt="dîner" />
-//               <p>19h30</p>
-//               <p className="accentText">Dîner </p>
-//               <small>(sur invitation)</small>
-//             </div>
-//             <div className="programme">
-//               <img src={discoball} alt="bal" />
-//               <p>22h00</p>
-//               <p className="accentText">Bal </p>
-//               <small>jusqu'à 6h !</small>
-//             </div>
-//           </div>
-//         </div>
       </div>
     </div>
   );
