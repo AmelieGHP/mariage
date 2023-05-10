cfb367c9736f7f1e71257cfa7285bc424d6c8371
Amélie Guilloux <109797891+AmelieGHP@users.noreply.github.com>
2023-05-10 20:10:43 +0200

    Create Photos.jsx

---
+++ src/pages/Photos.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Nav from "../components/Nav";
+import { TbHexagon } from "react-icons/tb";
+
+const Photos = () => {
+  return (
+    <div className="pageContent">
+      <div className="container programmeBox">
+        <h3>Photos </h3>
+        <div className="textContainer">
+          <p>
+            <TbHexagon className="noMargin" />
+            Nous avons créé un Drive Google pour regrouper toutes les photos de
+            notre grand jour !
+          </p>
+          <p>
+            <TbHexagon className="noMargin" />
+            Nous avons ajouté toutes les photos de la borne photo dans le
+            dossier correspondant, n'hésitez pas à les télécharger !
+          </p>
+          <p>
+            <TbHexagon className="noMargin" />
+            Vous pouvez consulter et télécharger les photos qui vous intéressent
+            et ajouter les votres dans le dossier "Photos des invités". Vous
+            devez avoir un compte Google et y être connecté pour pouvoir ajouter
+            vos images.
+          </p>
+          <p>
+            <TbHexagon className="noMargin" />
+            Attention : merci de ne supprimer aucune photo de ces dossiers car
+            elles seraient perdues pour nous également !
+          </p>
+          <p>
+            <TbHexagon className="noMargin" />
+            Pour accéder aux photos de la borne et des invités,{" "}
+            <a
+              href="https://drive.google.com/drive/folders/1IvUTN2E0sFet9n1koVl9Ygqqc4DjYKTj?usp=sharing"
+              target="_blank"
+            >
+              {" "}
+              cliquez ici
+            </a>
+            .
+          </p>
+          <p>
+            <TbHexagon className="noMargin" />
+            Nous sommes en attente des photos de notre photographe et
+            partagerons le lien vers sa galerie en ligne ici : elles seront
+            libres de téléchargement pour chacun.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Photos;
